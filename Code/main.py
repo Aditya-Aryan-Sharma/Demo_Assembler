da691341a169ebf6544d9babe9aa1673a40ada54
rahul20108 <rahul20108@iiitd.ac.in>
2021-08-12 01:35:59 +0530

    fixed permissions and environment setup

--- Code/main.py
+++
@@ -1,7 +0,0 @@
-from database import *
-
-s=input()
-s=s.split()
-#testing
-if (s[0]=="add"):
-    print(opcode["add"][0])
---
+++ Simple-Assembler/Code/main.py
@@ -0,0 +1,22 @@
+from database import *
+
+
+
+def filter_var_lables():
+
+
+
+def main():
+    global program
+    global program_counter
+    program=tuple(input().split(" "))#+(("plant","and"),)       temporary to take 1 line as input
+    print(program)
+    # while True:
+    #     try:
+    #         program=tuple(tuple(input().split(" ")).split("\n"))
+    #     except EOFError:
+    #         break
+    
+
+if __name__=="__main__":
+    main()
